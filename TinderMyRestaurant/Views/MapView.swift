cdada36191ce65065e7dd8cde53a0691e9fff241
Xinrong <xwen3@mail.sfsu.edu>
2021-12-06 20:07:48 -0800

    Added MapView, but need to fetch the userlocation

--- TinderMyRestaurant/Views/MapView.swift
+++ TinderMyRestaurant/Views/MapView.swift
@@ -6,3 +6,18 @@
 //
 
 import SwiftUI
+import MapKit
+
+struct MapView: View{
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054), span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
+    var body: some View{
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
