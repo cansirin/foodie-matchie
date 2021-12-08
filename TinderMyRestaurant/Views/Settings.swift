5f162e1f38bd6924d2c889d3ed91ec2391165f23
vdao182 <vdao1@mail.sfsu.edu>
2021-12-08 14:56:44 -0800

    changed layout of the settings page

--- TinderMyRestaurant/Views/Settings.swift
+++ TinderMyRestaurant/Views/Settings.swift
@@ -14,40 +14,49 @@ struct Settings: View {
     @State var sliderValue: Double = 0
     
     var body: some View {
-        List {
+        VStack{
             Section(header: Text("Account")){
                 
                 Image(systemName: "person").resizable().frame(width: 50, height: 50)
                 Text("Profile Setting").font(.largeTitle)
-            }.foregroundColor(.gray).frame(width: 300)
-            
+            }.foregroundColor(.purple).frame(width: 300)
+        
             
             VStack(){
                 Slider(value: $sliderValue, in: 0...20).accentColor(Color.yellow)
-                Text("Current distance from you: \(sliderValue, specifier: "%.2f")")
+                Text("Current distance from you: \(Int(sliderValue))")
                 
             }.padding()
-                .foregroundColor(Color.green)
+                .foregroundColor(Color.purple  )
             
-            Button("Save this distance", action: {fetcher.loadDocuMenu(latitude: locationManager.lastLocation.latitude, longitude: locationManager.lastLocation.longitude, distance: Int(sliderValue))} )
-            Section(){
+            VStack{
+                Button("Save this distance", action: {fetcher.loadDocuMenu(latitude: locationManager.lastLocation.latitude, longitude: locationManager.lastLocation.longitude, distance: Int(sliderValue))} ).foregroundColor(Color.white)
+                    
+                    
+            }.padding(10)
+                .background(Color.black)
+                .border(Color.yellow)
+                .cornerRadius(50)
+            Spacer()
+            HStack{
+                Section(){
                 Button("Sign out", action: {
                     sessionStore.signOut()
                 }).padding()
                     .font(.largeTitle)
                     .frame(alignment: .center)
-            }.padding()
-                .frame(alignment: .center)
-                
+                    .foregroundColor(Color.red)
+            }.frame( alignment: .center)
+                .background(RoundedRectangle(cornerRadius: 10, style: .continuous))
+            }
             
         }
     }
 }
-
-
-
-struct Settings_Previews: PreviewProvider {
-    static var previews: some View {
-        Settings(sessionStore: SessionStore(), fetcher: RestaurantFetcher(), locationManager: LocationManager())
-    }
-}
+                     
+                     
+                     struct Settings_Previews: PreviewProvider {
+                    static var previews: some View {
+                        Settings(sessionStore: SessionStore(), fetcher: RestaurantFetcher(), locationManager: LocationManager())
+                    }
+                }
